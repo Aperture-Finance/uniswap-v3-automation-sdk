305f81cacff1f0093865dceda2a85e67bf7d50e0
Peiqian Li <peiqian@aperture.finance>
2023-05-04 11:09:44 -0700

    Add typechain types for UniswapV3Factory.

--- typechain-types/factories/@uniswap/v3-core/contracts/interfaces/index.ts
+++ typechain-types/factories/@uniswap/v3-core/contracts/interfaces/index.ts
@@ -3,4 +3,5 @@
 /* eslint-disable */
 export * as callback from "./callback";
 export * as pool from "./pool";
+export { IUniswapV3Factory__factory } from "./IUniswapV3Factory__factory";
 export { IUniswapV3Pool__factory } from "./IUniswapV3Pool__factory";
