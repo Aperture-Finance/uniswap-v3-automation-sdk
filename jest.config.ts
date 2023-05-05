668e895f9fd684b0f80b5fff2ab356fb6012daca
while1 <gaohan137@gmail.com>
2023-05-05 15:13:17 -0700

    Create initial version of Automan client (#7)
    * Create initial version of Automan client
    
    * Bump up version for release

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
+  // A list of paths to directories that Jest should use to search for files in
+  // roots: ['./tests/unit', './tests/integration'],
+  // transform: {
+  //   '^.+\\.ts?$': 'esbuild-jest',
+  // },
+  clearMocks: true,
+};
