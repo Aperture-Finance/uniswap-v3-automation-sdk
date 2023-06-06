fd1ac532cc760b41df6887c1ffea803fa469aeeb
Peiqian Li <peiqian@aperture.finance>
2023-06-06 12:36:52 -0700

    Add  and  to TriggerItem. Remove unnecessary fields from LimitOrderClose request. Update fields in LimitOrderInfo.

--- automan_client.ts
+++ automan_client.ts
@@ -1,9 +1,12 @@
 import axios from 'axios';
+import stringify from 'json-stable-stringify';
 import {
+  CheckPositionPermitRequest,
   CreateTriggerRequest,
   DeleteTriggerRequest,
   ListTriggerRequest,
   ListTriggerResponse,
+  UpdatePositionPermitRequest,
 } from './interfaces';
 
 export class AutomanClient {
@@ -27,20 +30,34 @@ export class AutomanClient {
     request: Readonly<CreateTriggerRequest>,
   ): Promise<string> {
     const url = new URL('/Prod/createTrigger', this.endpoint);
-    return (await this.query(url, 'post', JSON.stringify(request))).data;
+    return (await this.query(url, 'post', stringify(request))).data;
   }
 
   async listTrigger(
     request: Readonly<ListTriggerRequest>,
   ): Promise<ListTriggerResponse> {
     const url = new URL('/Prod/listTrigger', this.endpoint);
-    return (await this.query(url, 'get', JSON.stringify(request))).data;
+    return (await this.query(url, 'get', stringify(request))).data;
   }
 
   async deleteTrigger(
     request: Readonly<DeleteTriggerRequest>,
   ): Promise<string> {
     const url = new URL('/Prod/deleteTrigger', this.endpoint);
-    return (await this.query(url, 'post', JSON.stringify(request))).data;
+    return (await this.query(url, 'post', stringify(request))).data;
+  }
+
+  async checkPositionApproval(
+    request: Readonly<CheckPositionPermitRequest>,
+  ): Promise<string> {
+    const url = new URL('/Prod/checkPositionApproval', this.endpoint);
+    return (await this.query(url, 'get', stringify(request))).data;
+  }
+
+  async updatePositionPermit(
+    request: Readonly<UpdatePositionPermitRequest>,
+  ): Promise<string> {
+    const url = new URL('/Prod/updatePositionPermit', this.endpoint);
+    return (await this.query(url, 'post', stringify(request))).data;
   }
 }
