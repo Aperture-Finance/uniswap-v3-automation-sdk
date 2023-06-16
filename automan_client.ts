6a92f26c2a384d06ecf17f352308ad7ba4359d15
Peiqian Li <peiqian@aperture.finance>
2023-06-16 11:17:09 -0700

    Update Automan client with the current request interface.

--- automan_client.ts
+++ automan_client.ts
@@ -11,69 +11,82 @@ import {
   UpdateTriggerRequest,
 } from './interfaces';
 
+async function buildAxiosGetRequest(
+  url: URL,
+  request: Readonly<
+    ListTriggerRequest | CheckPositionPermitRequest | CheckUserLimitRequest
+  >,
+) {
+  return axios.get(url.toString(), {
+    params: new URLSearchParams({
+      request: stringify(request),
+    }),
+  });
+}
+
+async function buildAxiosPostRequest(
+  url: URL,
+  request: Readonly<
+    | CreateTriggerRequest
+    | UpdateTriggerRequest
+    | DeleteTriggerRequest
+    | UpdatePositionPermitRequest
+  >,
+) {
+  return axios.post(url.toString(), request);
+}
+
 export class AutomanClient {
   constructor(private readonly endpoint: string) {
     if (endpoint == null || endpoint == '') throw 'Invalid endpoint url';
-  }
-
-  private async query(url: URL, method: string, request: string) {
-    const queryParams = new URLSearchParams({
-      params: request,
-    });
-
-    return axios({
-      method: method,
-      url: url.toString(),
-      params: queryParams,
-    });
   }
 
   async createTrigger(
     request: Readonly<CreateTriggerRequest>,
   ): Promise<string> {
     const url = new URL('/Prod/createTrigger', this.endpoint);
-    return (await this.query(url, 'post', stringify(request))).data;
+    return (await buildAxiosPostRequest(url, request)).data;
   }
 
   async listTrigger(
     request: Readonly<ListTriggerRequest>,
   ): Promise<ListTriggerResponse> {
     const url = new URL('/Prod/listTrigger', this.endpoint);
-    return (await this.query(url, 'get', stringify(request))).data;
+    return (await buildAxiosGetRequest(url, request)).data;
   }
 
   async updateTrigger(
     request: Readonly<UpdateTriggerRequest>,
   ): Promise<string> {
     const url = new URL('/Prod/updateTrigger', this.endpoint);
-    return (await this.query(url, 'post', stringify(request))).data;
+    return (await buildAxiosPostRequest(url, request)).data;
   }
 
   async deleteTrigger(
     request: Readonly<DeleteTriggerRequest>,
   ): Promise<string> {
     const url = new URL('/Prod/deleteTrigger', this.endpoint);
-    return (await this.query(url, 'post', stringify(request))).data;
+    return (await buildAxiosPostRequest(url, request)).data;
   }
 
   async checkPositionApproval(
     request: Readonly<CheckPositionPermitRequest>,
   ): Promise<string> {
     const url = new URL('/Prod/checkPositionApproval', this.endpoint);
-    return (await this.query(url, 'get', stringify(request))).data;
+    return (await buildAxiosGetRequest(url, request)).data;
   }
 
   async updatePositionPermit(
     request: Readonly<UpdatePositionPermitRequest>,
   ): Promise<string> {
     const url = new URL('/Prod/updatePositionPermit', this.endpoint);
-    return (await this.query(url, 'post', stringify(request))).data;
+    return (await buildAxiosPostRequest(url, request)).data;
   }
 
   async checkUserLimit(
     request: Readonly<CheckUserLimitRequest>,
   ): Promise<string> {
     const url = new URL('/Prod/checkUserLimit', this.endpoint);
-    return (await this.query(url, 'get', stringify(request))).data;
+    return (await buildAxiosGetRequest(url, request)).data;
   }
 }
