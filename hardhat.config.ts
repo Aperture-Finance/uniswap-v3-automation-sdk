1d4353432a86d7ea02813cbaa99c666225b16937
Shuhui Luo <shuhui@aperture.finance>
2023-07-17 05:06:19 -0700

    Add fork tests

---
+++ hardhat.config.ts
@@ -0,0 +1,18 @@
+import { config as dotenvConfig } from 'dotenv';
+import { HardhatUserConfig } from 'hardhat/config';
+
+dotenvConfig();
+
+const config: HardhatUserConfig = {
+  networks: {
+    hardhat: {
+      chainId: 1,
+      forking: {
+        blockNumber: 17188000,
+        url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      },
+    },
+  },
+};
+
+export default config;
