31c670e21b66884452bc3518ab993447ae903135
Shuhui Luo <shuhui@aperture.finance>
2023-07-24 19:01:46 -0700

    chore: update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,7 @@ module.exports = {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
     '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
   },
 };
