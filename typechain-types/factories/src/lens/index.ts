7840a4d1d57bccac61669330418a9851b6c99c62
Shuhui Luo <shuhui@aperture.finance>
2023-10-12 16:28:09 +0800

    Update TypeChain to add `EphemeralGetPositions`

--- typechain-types/factories/src/lens/index.ts
+++ typechain-types/factories/src/lens/index.ts
@@ -5,3 +5,4 @@ export { EphemeralAllPositions__factory } from "./EphemeralAllPositions__factory
 export { EphemeralGetPopulatedTicksInRange__factory } from "./EphemeralGetPopulatedTicksInRange__factory";
 export { EphemeralGetPopulatedTicksInWord__factory } from "./EphemeralGetPopulatedTicksInWord__factory";
 export { EphemeralGetPosition__factory } from "./EphemeralGetPosition__factory";
+export { EphemeralGetPositions__factory } from "./EphemeralGetPositions__factory";
