c73356f2f650476a582c2db5bb7599d5080ee7c5
Peiqian Li <peiqian@aperture.finance>
2024-02-26 14:31:14 +0800

    Update UserActivityTrackingRequestSchema.actionType.

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -74,16 +74,6 @@ export const ActionTypeEnum = z
   .describe('The type of action to take.');
 export type ActionTypeEnum = z.infer<typeof ActionTypeEnum>;
 
-// A more generic action type that includes action types beyond existing
-// automation offerings.
-export const GenericActionEnum = z.enum([
-  ...ActionTypeEnum.options,
-  'Swap',
-  'OpenPosition',
-  'AddLiquidity',
-]);
-export type GenericActionEnum = z.infer<typeof GenericActionEnum>;
-
 // TODO: Create a constant for the maximum allowed `maxGasProportion` that matches Automan setting and use it here.
 const MaxGasProportionSchema = z
   .number()
@@ -905,7 +895,8 @@ export const UserActivityTrackingRequestSchema = z.object({
   userAddress: AddressSchema,
   clientTimestampSecs: z.number().int().positive(),
   chainId: ApertureSupportedChainIdEnum,
-  actionType: GenericActionEnum,
+  // In the user activity DynamoDB table, action type can also be 'CreateTrigger' or 'ExecuteTrigger'; these two are added to the table by the backend trigger handlers as appropriate.
+  actionType: z.enum(['Swap', 'OpenPosition', 'AddLiquidity']),
   txHash: TxHashSchema,
   walletType: WalletTypeEnum,
   walletSubType: WalletConnectSubtypeEnum.optional(),
