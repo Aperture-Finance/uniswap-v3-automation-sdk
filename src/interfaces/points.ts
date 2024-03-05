cc6767fbf4e39f5bd054c82307ffe7520de8aba1
tommyzhao451 <tommy@aperture.finance>
2024-03-05 13:23:07 -0600

    updated leaderboard schema and version

--- src/interfaces/points.ts
+++ src/interfaces/points.ts
@@ -29,21 +29,15 @@ export const AcceptInviteRequestSchema = PayloadSignatureSchema.extend({
 export const LeaderboardUserResponseSchema = z.object({
   userAddr: z.string(),
   points: z.number().nonnegative(),
-  referred_users: z.array(LeaderboardUserResponseSchema),
+  referred_users: z.array(BasePayloadSchema),
 });
-
-export const ListLeaderboardRequestSchema = z.object({
-  // Since it can be pretty expensive to find top 1000 users and their referred users, good idea to cache
-  // to results to re-compute at most 1x every ~5 minutes.
-  // Seems like api caching can already be enabled https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/sam-resource-api.html
-});
-
-export type ListLeaderboardRequest = z.infer<
-  typeof LeaderboardRequestSchema
->;
 
 export const ListLeaderboardResponseSchema = z.object({
-  uers: z.array(LeaderboardUserResponseSchema).describe('Top 1000 users, their earned points, and their referred users'),
+  users: z
+    .array(LeaderboardUserResponseSchema)
+    .describe('Top 1000 users, their earned points, and their referred users'),
 });
 
-export type ListLeaderboardResponse = z.infer<typeof ListLeaderboardResponseSchema>;
+export type ListLeaderboardResponse = z.infer<
+  typeof ListLeaderboardResponseSchema
+>;
