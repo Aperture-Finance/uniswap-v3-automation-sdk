84828052f0dad797439132aa42951f2e06f5d08a
lanceaper <149023997+lanceaper@users.noreply.github.com>
2024-03-05 15:54:18 +0800

    Squashed commit of the following: (#164)
    commit c244d15dcb6a628bfcb4e2d5d37ac486ac0729da
    Author: Junlin <junlinwu@microsoft.com>
    Date:   Tue Mar 5 15:42:21 2024 +0800
    
        revert
    
    commit cfa37b4bfe960e35d1722248b627df8835018fca
    Author: Junlin <junlinwu@microsoft.com>
    Date:   Tue Mar 5 15:38:48 2024 +0800
    
        refine
    
    commit 6aac6c7441d295c87103cea3e910cbe46f6f826d
    Author: Junlin Wu <junlin@aperture.finance>
    Date:   Tue Mar 5 06:29:29 2024 +0000
    
        logs
    
    commit f3d531ce57703bf9247a50983975fe57d901e470
    Author: wujunlin <junlin@aperture.finance>
    Date:   Mon Mar 4 14:39:56 2024 +0800
    
        bump up package version
    
    commit a3b6fc04c6ab72dbc69ed6abf205587e699baed0
    Author: wujunlin <junlin@aperture.finance>
    Date:   Mon Mar 4 14:30:33 2024 +0800
    
        catch more 1inch error
    
    commit 0b77f7d84dc6d4457a9329fd019d9bb81c3c7f50
    Author: lanceaper <149023997+lanceaper@users.noreply.github.com>
    Date:   Tue Mar 5 10:59:08 2024 +0800
    
        remove log (#161)
    
        Co-authored-by: Junlin <junlinwu@microsoft.com>
    
    Co-authored-by: Junlin <junlinwu@microsoft.com>

--- src/viem/aggregator/optimalRebalance.ts
+++ src/viem/aggregator/optimalRebalance.ts
@@ -1,6 +1,5 @@
 import { ApertureSupportedChainId, getChainInfo } from '@/index';
 import { FeeAmount } from '@uniswap/v3-sdk';
-import Big from 'big.js';
 import { Address, Hex, PublicClient } from 'viem';
 
 import {
@@ -133,13 +132,13 @@ async function optimalMintPool(
     undefined,
     blockNumber,
   );
+
   let swapRoute: SwapRoute = [];
-  if (mintParams.amount0Desired.toString() !== amount0.toString()) {
-    const [fromTokenAddress, toTokenAddress] = new Big(
-      mintParams.amount0Desired.toString(),
-    ).gt(amount0.toString())
-      ? [mintParams.token0, mintParams.token1]
-      : [mintParams.token1, mintParams.token0];
+  if (mintParams.amount0Desired !== amount0) {
+    const [fromTokenAddress, toTokenAddress] =
+      mintParams.amount0Desired > amount0
+        ? [mintParams.token0, mintParams.token1]
+        : [mintParams.token1, mintParams.token0];
     swapRoute = [
       [
         [
