5f891a8a198519e94abede5f1268a273ed3ef657
wujunlin <junlin@aperture.finance>
2024-03-08 12:11:39 +0800

    PointUserStatusResponseSchema

--- src/interfaces/points.ts
+++ src/interfaces/points.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-import { PayloadSignatureSchema } from './interfaces';
+import { GeneralResponseSchema, PayloadSignatureSchema } from './interfaces';
 
 const SocialPlatformEnum = z.enum(['discord', 'twitter', 'telegram']);
 export type E_SocialPlatform = z.infer<typeof SocialPlatformEnum>;
@@ -44,4 +44,20 @@ export const ListLeaderboardResponseSchema = z.object({
 
 export type ListLeaderboardResponse = z.infer<
   typeof ListLeaderboardResponseSchema
+>;
+
+export const PointUserStatusResponseSchema = GeneralResponseSchema.extend({
+  userAddress: z.string(),
+  inviteCode: z.number().nonnegative(),
+  xBound: z.boolean(),
+  discordBound: z.boolean(),
+  telegramBound: z.boolean(),
+  referer: z.string(),
+  referredCount: z.number().int(),
+  points: z.number().nonnegative(),
+  referalPoints: z.number().nonnegative(),
+});
+
+export type PointUserStatusResponse = z.infer<
+  typeof PointUserStatusResponseSchema
 >;
