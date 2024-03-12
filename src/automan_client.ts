5b9616a99b05159401ed3a5276a156f40b104421
Junlin <junlinwu@microsoft.com>
2024-03-12 15:37:52 +0800

    automan helper

--- src/automan_client.ts
+++ src/automan_client.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 import {
+  AcceptInviteRequest,
   CheckPositionPermitRequest,
   CheckUserLimitRequest,
   CreateTriggerRequest,
@@ -14,10 +15,12 @@ import {
   ListLeaderboardResponse,
   ListTriggerRequest,
   ListTriggerResponse,
+  RaffleRequest,
   SignPrivateBetaAgreementRequest,
   UpdatePositionPermitRequest,
   UpdateTriggerRequest,
   UserActivityTrackingRequest,
+  VerifySocialAccountRequest,
   WalletTrackingRequest,
 } from './interfaces';
 
@@ -49,9 +52,17 @@ async function buildAxiosPostRequest(
     | SignPrivateBetaAgreementRequest
     | WalletTrackingRequest
     | UserActivityTrackingRequest
+    | VerifySocialAccountRequest
+    | AcceptInviteRequest
+    | RaffleRequest
   >,
 ) {
-  return axios.post(url.toString(), request);
+  return axios.post(url.toString(), request).catch((e) => {
+    if (e?.data?.error) {
+      throw e.data;
+    }
+    throw e;
+  });
 }
 
 export class AutomanClient {
@@ -151,5 +162,19 @@ export class AutomanClient {
   async listLeaderboard(): Promise<ListLeaderboardResponse> {
     const url = new URL('/listLeaderboard', this.endpoint);
     return (await buildAxiosGetRequest(url, null)).data;
+  }
+
+  async verifySocialAccount(
+    request: Readonly<VerifySocialAccountRequest>,
+  ): Promise<string> {
+    const url = new URL('/verifySocialAccount', this.endpoint);
+    return (await buildAxiosPostRequest(url, request)).data;
+  }
+
+  async acceptInvitation(
+    request: Readonly<AcceptInviteRequest>,
+  ): Promise<string> {
+    const url = new URL('/acceptInvitation', this.endpoint);
+    return (await buildAxiosPostRequest(url, request)).data;
   }
 }
