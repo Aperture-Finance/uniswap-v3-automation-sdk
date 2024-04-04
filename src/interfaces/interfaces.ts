9801db0bf5fe1a34f7ec1f21b861ca591b0f1a8c
gnarlycow <peiqian@aperture.finance>
2024-04-04 11:16:23 -0700

    Remove amm from getStrategyDetailRequest. (#197)
    * Remove amm from getStrategyDetailRequest.
    
    * refactor

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -608,6 +608,7 @@ export const UpdateTriggerRequestSchema = PayloadSignatureSchema.extend({
 export type UpdateTriggerRequest = z.infer<typeof UpdateTriggerRequestSchema>;
 
 export const ListTriggerRequestSchema = BaseTriggerPayloadSchema.extend({
+  amm: AutomatedMarketMakerEnum,
   isLimitOrder: z
     .boolean()
     .describe(
@@ -739,19 +740,23 @@ export type HasSignedPrivateBetaAgreementResponse = z.infer<
   typeof HasSignedPrivateBetaAgreementResponseSchema
 >;
 
-export const GetStrategiesDetailRequestSchema = ClientTypeSchema.extend({
+const GetStrategyRequestBaseSchema = ClientTypeSchema.extend({
   ownerAddr: AddressSchema.describe(
-    'The owner address of position `tokenId`; must be a checksum address.',
+    'The owner address of the request strategy/strategies; must be a checksum address.',
   ),
   chainId: ApertureSupportedChainIdEnum,
-  amm: AutomatedMarketMakerEnum,
 });
+
+export const GetStrategiesDetailRequestSchema =
+  GetStrategyRequestBaseSchema.extend({
+    amm: AutomatedMarketMakerEnum,
+  });
 export type GetStrategiesDetailRequest = z.infer<
   typeof GetStrategiesDetailRequestSchema
 >;
 
 export const GetStrategyDetailRequestSchema =
-  GetStrategiesDetailRequestSchema.extend({
+  GetStrategyRequestBaseSchema.extend({
     strategyId: z.string().min(1).describe('The id of the strategy.'),
   });
 export type GetStrategyDetailRequest = z.infer<
