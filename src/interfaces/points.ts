9777f866f9e63a75511119691a3465d5fc374d25
tommyzhao451 <tommy@aperture.finance>
2024-04-25 07:47:05 -0700

    update dates for phase3

--- src/interfaces/points.ts
+++ src/interfaces/points.ts
@@ -22,9 +22,6 @@ export const RafflePrizeEnum = z.enum([
 ]);
 export type RafflePrizeEnum = z.infer<typeof RafflePrizeEnum>;
 
-export const LeaderboardTypeEnum = z.enum(['points', 'referrals', 'streak']);
-export type LeaderboardTypeEnum = z.infer<typeof LeaderboardTypeEnum>;
-
 const BasePayloadSchema = z.object({
   ownerAddr: z.string(),
 });
@@ -89,15 +86,6 @@ export type ValidateInviteCodeRequest = z.infer<
   typeof ValidateInviteCodeRequestSchema
 >;
 
-export const ListLeaderboardRequestSchema = z.object({
-  type: LeaderboardTypeEnum.describe(
-    'Type of leaderboard for sorting.',
-  ).optional(),
-});
-export type ListLeaderboardRequest = z.infer<
-  typeof ListLeaderboardRequestSchema
->;
-
 export const LeaderboardUserResponseSchema = z.object({
   x_id: z.string(),
   userAddr: z.string(),
@@ -107,12 +95,10 @@ export const LeaderboardUserResponseSchema = z.object({
 });
 
 export const ListLeaderboardResponseSchema = z.object({
-  totalCampaignUsers: z.number().int().nonnegative(),
-  totalCampaignPoints: z.number().nonnegative(),
   users: z
     .array(LeaderboardUserResponseSchema)
     .describe(
-      "List top 1000 wallet's { address, points, numReferrals, streak } ordered by type.",
+      'Lists top users and their earned points, number of referred users, and x_id',
     ),
 });
 
