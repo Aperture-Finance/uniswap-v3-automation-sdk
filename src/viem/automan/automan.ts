d7001ad4a7be8a71e610aaec3f8491ebb16ba8c5
Junlin <junlinwu@microsoft.com>
2024-04-27 11:50:06 +0800

    remove log

--- src/viem/automan/automan.ts
+++ src/viem/automan/automan.ts
@@ -323,10 +323,6 @@ export async function simulateMintOptimal(
       blockNumber,
     );
   } catch (e) {
-    if (process.env.NODE_ENV === 'development') {
-      console.log('fail to override', e);
-    }
-
     returnData = (
       await publicClient.call({
         account: from,
