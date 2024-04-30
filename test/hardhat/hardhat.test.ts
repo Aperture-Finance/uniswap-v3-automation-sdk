faa86eb1ac1422d81a7a644ea4d730e5abf14807
Junlin <junlinwu@microsoft.com>
2024-04-30 15:28:36 +0800

    fix test

--- test/hardhat/hardhat.test.ts
+++ test/hardhat/hardhat.test.ts
@@ -75,9 +75,6 @@ import {
 } from '../../src';
 import {
   PositionDetails,
-  calculateIncreaseLiquidityOptimalPriceImpact,
-  calculateMintOptimalPriceImpact,
-  calculateRebalancePriceImpact,
   checkPositionApprovalStatus,
   computeOperatorApprovalSlot,
   estimateRebalanceGas,
@@ -393,21 +390,6 @@ describe('State overrides tests', function () {
       deadline: BigInt(Math.floor(Date.now() / 1000 + 60 * 30)),
     };
 
-    const { priceImpact, finalAmount0, finalAmount1 } =
-      await calculateMintOptimalPriceImpact({
-        chainId,
-        amm: UNIV3_AMM,
-        publicClient,
-        from: eoa,
-        mintParams,
-        swapData: undefined,
-        blockNumber,
-      });
-
-    expect(priceImpact.toString()).to.equal('0.003010298098311076209996397317');
-    expect(finalAmount0.toString()).to.equal('51320357');
-    expect(finalAmount1.toString()).to.equal('8736560293857784398');
-
     const [, liquidity, amount0, amount1] = await simulateMintOptimal(
       chainId,
       UNIV3_AMM,
@@ -443,22 +425,6 @@ describe('State overrides tests', function () {
       amount1Min: BigInt(0),
       deadline: BigInt(Math.floor(Date.now() / 1000 + 60 * 30)),
     };
-
-    const { priceImpact, finalAmount0, finalAmount1 } =
-      await calculateIncreaseLiquidityOptimalPriceImpact({
-        chainId,
-        amm: UNIV3_AMM,
-        publicClient,
-        from: eoa,
-        position,
-        increaseParams,
-        swapData: undefined,
-        blockNumber,
-      });
-
-    expect(priceImpact.toString()).to.equal('0.00300826277866017098015215935');
-    expect(finalAmount0.toString()).to.equal('61259538');
-    expect(finalAmount1.toString()).to.equal('7156958298534991565');
 
     const [, amount0, amount1] = await simulateIncreaseLiquidityOptimal(
       chainId,
@@ -526,62 +492,6 @@ describe('State overrides tests', function () {
     expect(priceImpact.toString()).to.equal('0');
     expect(finalAmount0.toString()).to.equal('96674');
     expect(finalAmount1.toString()).to.equal('16468879195954429');
-  });
-
-  it('Test calculateRebalancePriceImpact', async function () {
-    const blockNumber = 17975698n;
-    const publicClient = getInfuraClient();
-
-    const token0 = WBTC_ADDRESS;
-    const token1 = WETH_ADDRESS;
-    const fee = FeeAmount.MEDIUM;
-    const amount0Desired = 100000000n;
-    const amount1Desired = 1000000000000000000n;
-    const pool = await getPool(
-      token0,
-      token1,
-      fee,
-      chainId,
-      UNIV3_AMM,
-      publicClient,
-      blockNumber,
-    );
-    const mintParams = {
-      token0: token0 as Address,
-      token1: token1 as Address,
-      fee,
-      tickLower: nearestUsableTick(
-        pool.tickCurrent - 10 * pool.tickSpacing,
-        pool.tickSpacing,
-      ),
-      tickUpper: nearestUsableTick(
-        pool.tickCurrent + 10 * pool.tickSpacing,
-        pool.tickSpacing,
-      ),
-      amount0Desired,
-      amount1Desired,
-      amount0Min: BigInt(0),
-      amount1Min: BigInt(0),
-      recipient: eoa as Address,
-      deadline: BigInt(Math.floor(Date.now() / 1000 + 60 * 30)),
-    };
-
-    const { priceImpact, finalAmount0, finalAmount1 } =
-      await calculateRebalancePriceImpact({
-        chainId,
-        amm: UNIV3_AMM,
-        publicClient,
-        from: eoa,
-        owner: eoa,
-        mintParams,
-        tokenId: 4n,
-        feeBips: undefined,
-        swapData: undefined,
-        blockNumber,
-      });
-    expect(priceImpact.toString()).to.equal('0.00300526535105717178193071153');
-    expect(finalAmount0.toString()).to.equal('23718330');
-    expect(finalAmount1.toString()).to.equal('4040287637285704807');
   });
 });
 
@@ -1674,7 +1584,6 @@ describe('Automan transaction tests', function () {
       tickLower,
       tickUpper,
       eoa,
-      BigInt(Math.floor(Date.now() / 1000) + 60),
       0.5,
       publicClient,
       // 1inch quote currently doesn't support the no-swap case.
@@ -1732,7 +1641,6 @@ describe('Automan transaction tests', function () {
       tickLower,
       tickUpper,
       eoa,
-      BigInt(Math.floor(Date.now() / 1000) + 60),
       0.5,
       publicClient,
       // 1inch quote currently doesn't support the no-swap case.
