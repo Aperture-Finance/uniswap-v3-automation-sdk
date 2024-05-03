aa791026b6cdfc6479c83f5026a20fa07cfe0fc2
Junlin <junlinwu@microsoft.com>
2024-05-03 09:56:28 +0800

    enum solver

--- src/viem/aggregator/types.ts
+++ src/viem/aggregator/types.ts
@@ -2,6 +2,12 @@ import { Address } from 'viem';
 
 import { E_Solver, SwapRoute } from '../solver';
 
+export enum E_Solver {
+  PH = 'PropellerHeads',
+  UNISWAP = 'Uniswap',
+  OneInch = '1Inch',
+}
+
 export type SolverResult = {
   solver?: E_Solver; // TODO: make it required
   amount0: bigint;
