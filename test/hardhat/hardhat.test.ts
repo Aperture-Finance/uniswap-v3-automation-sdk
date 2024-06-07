5cea0ab3047dcf1f231c405ea620f8ec813c5b79
wujunlin <junlin@aperture.finance>
2024-06-07 10:55:23 +0800

    test

--- test/hardhat/hardhat.test.ts
+++ test/hardhat/hardhat.test.ts
@@ -828,7 +828,7 @@ describe('Position util tests', function () {
     );
   });
 
-  it('Test getAllPositions', async function () {
+  it.skip('Test getAllPositions', async function () {
     const publicClient = getPublicClient(1);
     // an address with 90+ positions
     const address = '0xD68C7F0b57476D5C9e5686039FDFa03f51033a4f';
@@ -868,7 +868,7 @@ describe('Position util tests', function () {
     }
   });
 
-  it('Test getAllPositions with large balances', async function () {
+  it.skip('Test getAllPositions with large balances', async function () {
     const publicClient = getPublicClient(1, 'https://ethereum.publicnode.com');
     // An address with 7000+ positions on mainnet.
     const address = '0x6dD91BdaB368282dc4Ea4f4beFc831b78a7C38C0';
