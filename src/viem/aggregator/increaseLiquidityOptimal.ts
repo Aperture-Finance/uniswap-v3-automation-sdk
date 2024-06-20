4e57453e6a1dcd946aa1264d745f6381bf4f1b68
Junlin <junlinwu@microsoft.com>
2024-06-20 12:37:55 +0800

    revert

--- src/viem/aggregator/increaseLiquidityOptimal.ts
+++ src/viem/aggregator/increaseLiquidityOptimal.ts
@@ -82,7 +82,7 @@ export async function increaseLiquidityOptimal(
         fromAddress,
         position,
         increaseParams,
-        Number(increaseOptions.slippageTolerance.toFixed()),
+        Number(increaseOptions.slippageTolerance.toSignificant()),
       ),
     ]);
     // use the same pool if the quote isn't better
@@ -113,7 +113,7 @@ export async function increaseLiquidityOptimal(
       BigInt(token1Amount.quotient.toString()),
       ret.amount0,
       ret.amount1,
-      Number(increaseOptions.slippageTolerance.toFixed()),
+      Number(increaseOptions.slippageTolerance.toSignificant()),
     );
   }
 
