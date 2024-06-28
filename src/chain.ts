16cf1bdd5ab39625cca3769d13ef77469d66e371
wujunlin <junlin@aperture.finance>
2024-06-28 14:31:07 +0800

    update

--- src/chain.ts
+++ src/chain.ts
@@ -424,7 +424,7 @@ const CHAIN_ID_TO_INFO: {
       'Wrapped Ether',
     ),
     coingecko_asset_platform_id: 'scroll',
-    rpc_url: 'https://scroll.drpc.org',
+    rpc_url: 'https://scroll-mainnet.chainstacklabs.com',
     maxGasCeiling: 0.2,
     routingApiInfo: {
       type: 'ROUTING_API',
