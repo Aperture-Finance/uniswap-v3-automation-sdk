f9167b6aed026eb66db5b0200f26f48b1eaf113d
tommyzhao451 <tommy@aperture.finance>
2024-07-02 15:30:20 -0700

    extend expiration

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,6 @@ export const PRIVATE_BETA_MSG = 'I would like to opt into the private beta.';
 export const GET_APTR_AIRDROP_STATUS_MSG = 'Get APTR Airdrop Status at '; // Prepend to timestampSecs for users to sign to validate getAptrAirdropStatus().
 export const HOUR_IN_SECS = 60 * 60; // 60 minutes of 60 seconds each.
 export const DAY_IN_SECS = 24 * HOUR_IN_SECS;
-export const AUTOMATION_EXPIRATION_IN_DAYS = 60; // For frontend.
+export const AUTOMATION_EXPIRATION_IN_DAYS = 90; // For frontend.
 export const AUTOMATION_EXPIRATION_IN_SECS = // For backend.
   AUTOMATION_EXPIRATION_IN_DAYS * DAY_IN_SECS;
