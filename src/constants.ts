cef9c72beab036212191efe08e66bb27f171716b
tommyzhao451 <tommy@aperture.finance>
2024-07-01 17:35:27 -0700

    update consts for frontend

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const HOUR_IN_SECONDS = 60 * 60; // 60 minutes of 60 seconds each.
 export const DAY_IN_SECONDS = 24 * HOUR_IN_SECONDS;
-export const AUTOMATION_EXPIRATION_IN_SECONDS = 60 * DAY_IN_SECONDS;
+export const AUTOMATION_EXPIRATION_IN_DAYS = 60; // For frontend.
+export const AUTOMATION_EXPIRATION_IN_SECONDS = AUTOMATION_EXPIRATION_IN_DAYS * DAY_IN_SECONDS; // For backend.
