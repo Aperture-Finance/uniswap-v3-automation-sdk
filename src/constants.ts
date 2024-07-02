ce1d06f37c7eae3f73860d61dbd95f7fcb951af3
tommyzhao451 <tommy@aperture.finance>
2024-07-01 17:36:18 -0700

    shorten const names

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const HOUR_IN_SECONDS = 60 * 60; // 60 minutes of 60 seconds each.
-export const DAY_IN_SECONDS = 24 * HOUR_IN_SECONDS;
+export const HOUR_IN_SECS = 60 * 60; // 60 minutes of 60 seconds each.
+export const DAY_IN_SECS = 24 * HOUR_IN_SECS;
 export const AUTOMATION_EXPIRATION_IN_DAYS = 60; // For frontend.
-export const AUTOMATION_EXPIRATION_IN_SECONDS = AUTOMATION_EXPIRATION_IN_DAYS * DAY_IN_SECONDS; // For backend.
+export const AUTOMATION_EXPIRATION_IN_SECS = AUTOMATION_EXPIRATION_IN_DAYS * DAY_IN_SECS; // For backend.
