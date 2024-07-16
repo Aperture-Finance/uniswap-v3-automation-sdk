485f4894864f90eab2c7799890134a62826036fe
tommyzhao451 <tommy@aperture.finance>
2024-07-15 18:25:08 -0700

    add slack time

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,7 @@
 export const HOUR_IN_SECS = 60 * 60; // 60 minutes of 60 seconds each.
 export const DAY_IN_SECS = 24 * HOUR_IN_SECS;
 export const AUTOMATION_EXPIRATION_IN_DAYS = 90; // For frontend.
+// Allow some slack time because frontend users' clock might not be synced.
+export const AUTOMATION_SLACK_IN_SECS = Number(process.env.AUTOMATION_SLACK_IN_SECS || 1 * DAY_IN_SECS);
 export const AUTOMATION_EXPIRATION_IN_SECS = // For backend.
-  AUTOMATION_EXPIRATION_IN_DAYS * DAY_IN_SECS;
+  AUTOMATION_EXPIRATION_IN_DAYS * DAY_IN_SECS + AUTOMATION_SLACK_IN_SECS;
