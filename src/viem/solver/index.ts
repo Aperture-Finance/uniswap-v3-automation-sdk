64333e2830515d0bf1fe7874c00d77d443b3268d
tommyzhao451 <tommy@aperture.finance>
2024-09-10 21:10:17 -0700

    added back in optimalRebalanceV1

--- src/viem/solver/index.ts
+++ src/viem/solver/index.ts
@@ -10,6 +10,7 @@ export * from './increaseLiquidityOptimal';
 export * from './increaseLiquidityOptimalV2';
 export * from './optimalMint';
 export * from './optimalMintV2';
+export * from './optimalRebalance';
 export * from './optimalRebalanceV2';
 export * from './types';
 
