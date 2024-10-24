1945f5c48779d7300bec134bd99cfbe128f0de7d
tommyzhao451 <tommy@aperture.finance>
2024-10-24 15:39:14 -0700

    update overrides for consistency, add param names for undefines

--- src/viem/mmvault/mmvault.ts
+++ src/viem/mmvault/mmvault.ts
@@ -90,7 +90,7 @@ export async function requestMMVaultRebalance<M extends keyof RpcReturnType>(
       data,
     },
     publicClient,
-    /* overrides= */ {},
+    /* overrides= */ undefined,
     blockNumber,
   );
 }
