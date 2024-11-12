0fb501597774167f9886e8ae073d38a5b389fe49
Junlin Wu <junlin@aperture.finance>
2024-11-12 20:08:50 +0800

    update

--- src/viem/solver/common.ts
+++ src/viem/solver/common.ts
@@ -1,6 +1,6 @@
 import Bottleneck from 'bottleneck';
 
 export const limiter = new Bottleneck({
-  maxConcurrent: 2, // Number of concurrent promises
-  minTime: 0, // Minimum time (in ms) between the start of subsequent promises
+  maxConcurrent: 3, // Number of concurrent promises
+  minTime: 250, // Minimum time (in ms) between the start of subsequent promises
 });
