f4999815fda43b2f7184a62cca6c41258167db8b
jiaqi-aperture <jiaqi@aperture.finance>
2025-03-17 15:37:12 -0700

    fix: reinvest build err

--- src/viem/transaction/getReinvestTx.ts
+++ src/viem/transaction/getReinvestTx.ts
@@ -1,13 +1,106 @@
-import { ApertureSupportedChainId, PermitInfo, getAMMInfo } from '@/index';
+import {
+  ApertureSupportedChainId,
+  PermitInfo,
+  getAMMInfo,
+  getLogger,
+} from '@/index';
 import { IncreaseOptions } from '@aperture_finance/uniswap-v3-sdk';
+import { Percent } from '@uniswap/sdk-core';
 import { AutomatedMarketMakerEnum } from 'aperture-lens/dist/src/viem';
-import { Address, Hex, TransactionRequest } from 'viem';
+import { Address, Hex, PublicClient, TransactionRequest } from 'viem';
 
 import {
   IncreaseLiquidityParams,
   getAutomanReinvestCalldata,
+  getAutomanReinvestNoNeedSwapCalldata,
   getAutomanV3ReinvestCalldata,
 } from '../automan';
+import { getFeeReinvestBips } from '../automan/getFees';
+import { PositionDetails } from '../position';
+import { getAmountsWithSlippage } from './transaction';
+import { SimulatedAmounts } from './types';
+
+/**
+ * Generates an unsigned tx that collects fees and reinvests into the specified position.
+ * @param chainId Chain id.
+ * @param amm Automated Market Maker.
+ * @param ownerAddress Owner of the specified position.
+ * @param positionId Position id.
+ * @param slippageTolerance How much the reinvested amount of either token0 or token1 is allowed to change unfavorably.
+ * @param deadlineEpochSeconds Timestamp when the tx expires (in seconds since epoch).
+ * @param client Public client.
+ * @param permitInfo Optional. If Automan doesn't already have authority over the existing position, this should be populated with a valid owner-signed permit info.
+ * @returns The generated transaction request and expected amounts.
+ */
+export async function getReinvestNoNeedSwapTx(
+  chainId: ApertureSupportedChainId,
+  amm: AutomatedMarketMakerEnum,
+  ownerAddress: Address,
+  positionId: bigint,
+  slippageTolerance: Percent,
+  deadlineEpochSeconds: bigint,
+  client: PublicClient,
+  permitInfo?: PermitInfo,
+): Promise<{
+  tx: TransactionRequest;
+  amounts: SimulatedAmounts;
+}> {
+  const positionDetails = await PositionDetails.fromPositionId(
+    chainId,
+    amm,
+    positionId,
+    client,
+  );
+  const { pool, tickLower, tickUpper, position } = positionDetails;
+  const { apertureAutoman } = getAMMInfo(chainId, amm)!;
+
+  const feeBips = getFeeReinvestBips(positionDetails);
+  getLogger().info('SDK.getReinvestTx.Fees', {
+    ownerAddress,
+    amm,
+    chainId,
+    positionId,
+    collectableToken0: positionDetails.tokensOwed0.toSignificant(),
+    collectableToken1: positionDetails.tokensOwed1.toSignificant(),
+    positionToken0: position.amount0.toSignificant(),
+    positionToken1: position.amount1.toSignificant(),
+    feeBips,
+  });
+  const data = getAutomanReinvestNoNeedSwapCalldata(
+    positionId,
+    deadlineEpochSeconds,
+    0n /*amount0Min*/, // Setting this to zero for tx simulation.
+    0n /*amount1Min*/, // Setting this to zero for tx simulation.
+    feeBips,
+    permitInfo,
+  );
+  const amounts = await getAmountsWithSlippage(
+    pool,
+    tickLower,
+    tickUpper,
+    apertureAutoman,
+    ownerAddress,
+    'reinvest',
+    data,
+    slippageTolerance,
+    client,
+  );
+  return {
+    tx: {
+      from: ownerAddress,
+      to: apertureAutoman,
+      data: getAutomanReinvestNoNeedSwapCalldata(
+        positionId,
+        deadlineEpochSeconds,
+        BigInt(amounts.amount0Min),
+        BigInt(amounts.amount1Min),
+        feeBips,
+        permitInfo,
+      ),
+    },
+    amounts,
+  };
+}
 
 /**
  * Generates an unsigned tx that collects fees and reinvests into the specified position.
